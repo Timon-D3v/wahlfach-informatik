755d94b93a094235361f49b04d02e4f89b0910b7
Timon Fiedler <fiedlertimon@gmail.com>
2024-12-04 19:14:13 +0100

    # 2. Commit vom 4.12.24 # Made the necessary changes to the sorting algorithm => Now displaying the two compared values red

--- 07_Bubble Sorting visualisieren.py
+++ 07_Bubble Sorting visualisieren.py
@@ -11,7 +11,7 @@ class BubbleSortVisualizer:
         self.rects = []
         self.draw_array()
 
-    def draw_array(self):
+    def draw_array(self, rect_1=0, rect_2=1):
         self.canvas.delete("all")
         bar_width = 500 / len(self.array)
         for i, val in enumerate(self.array):
@@ -21,12 +21,14 @@ class BubbleSortVisualizer:
             y1 = 500
             rect = self.canvas.create_rectangle(x0, y0, x1, y1, fill="blue")
             self.rects.append(rect)
+            if (i == rect_1 or i == rect_2):
+                self.canvas.itemconfig(rect, fill="red")
 
     def bubble_sort_step(self):
         for i in range(len(self.array) - 1):
             if self.array[i] > self.array[i + 1]:
                 self.array[i], self.array[i + 1] = self.array[i + 1], self.array[i]
-                self.draw_array()
+                self.draw_array(i, i + 1)
                 self.root.after(10, self.bubble_sort_step)
                 return
         self.root.after(10, self.bubble_sort_step)
