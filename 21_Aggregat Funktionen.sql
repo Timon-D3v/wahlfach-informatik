846d901e0346c3f40b2888ebb7f415b55725a90b
Timon-D3v <fiedlertimon@gmail.com>
2025-03-14 08:46:20 +0100

    Commit vom 7. März

--- 21_Aggregat Funktionen.sql
+++ 21_Aggregat Funktionen.sql
@@ -1,7 +1,7 @@
-SELECT * from countries ORDER BY gdp DESC LIMIT 1;
+SELECT * FROM countries ORDER BY gdp DESC LIMIT 1;
 
 
-SELECT SUM(population) AS "Gesamtbevölkerung", AVG(area) as "Durchschnittsfläche" FROM countries;
+SELECT SUM(population) AS "Gesamtbevölkerung", AVG(area) AS "Durchschnittsfläche" FROM countries;
 
 
 SELECT subregion, SUM(gdp) AS "Gesamt BIP" FROM countries GROUP BY subregion;
@@ -16,7 +16,11 @@ SELECT COUNT(DISTINCT continent) AS "Anzahl Kontinente", COUNT(DISTINCT subregio
 SELECT subregion, AVG(gdp) AS "Durchschnittlicher BIP" FROM countries GROUP BY subregion HAVING COUNT(*) > 15;
 
 
-SELECT `year`, COUNT(DISTINCT name) from babyname GROUP BY `year`;
+SELECT `year`, COUNT(DISTINCT name) FROM babyname GROUP BY `year`;
 
 
-SELECT sex, COUNT(DISTINCT name) from babyname WHERE `year` = 2000 GROUP BY sex;
+SELECT sex, COUNT(DISTINCT name) FROM babyname WHERE `year` = 2000 GROUP BY sex;
+
+SELECT `year`, COUNT(DISTINCT name) FROM babyname GROUP BY year ORDER BY COUNT(DISTINCT name) DESC LIMIT 1;
+
+SELECT year, name, birth_count FROM babyname WHERE birth_count > 20 ORDER BY birth_count DESC;
